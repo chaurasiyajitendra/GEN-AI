08dee0a6d87f1c338fe7238ed555fad50f80a39e
Jitendra <jitendragta902@gmail.com>
2026-06-06 20:33:32 +0530

    Fix App.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,3 +1,13 @@
+if (typeof global.DOMMatrix === 'undefined') {
+    global.DOMMatrix = class DOMMatrix {};
+}
+if (typeof global.ImageData === 'undefined') {
+    global.ImageData = class ImageData {};
+}
+if (typeof global.Path2D === 'undefined') {
+    global.Path2D = class Path2D {};
+}
+
 const express = require("express");
 const app = express();
 const cookie = require('cookie-parser');
